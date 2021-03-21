72f605d600c6abad03b7b1d3a4cb0116de6268b8
Markus <markus.randa@spillfree.no>
2021-03-21 16:54:29 +0100

    Added new method ReadAndPrintAllFiles

--- pkg/filemanager/filemanager.go
+++ pkg/filemanager/filemanager.go
@@ -77,6 +77,20 @@ func ReadAndPrintFiles(dir string) {
     }
 }
 
+func ReadAndPrintAllFiles(dir string) {
+    err := filepath.Walk(dir,
+    func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+        return err
+    }
+    fmt.Println(path, info.Size())
+        return nil  
+    })  
+    if err != nil {
+        log.Println(err)
+    }
+}
+
 func getFilesFromDir(dir string) (files []os.FileInfo) {
 	files, err := ioutil.ReadDir(dir)
     if err != nil {
